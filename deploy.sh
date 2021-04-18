002b934332703db64648c49762c18ef36ad6cdad
Hamza <hamzazahidulislam444@gmail.com>
2021-04-18 03:57:21 +0300

    /

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
-docker build -t hamzazahid/multi-client -f ./client/Dockerfile ./client
-docker build -t hamzazahid/multi-server -f ./server/Dockerfile ./server
-docker build -t hamzazahid/multi-worker -f ./worker/Dockerfile ./worker
-docker push hamzazahid/multi-client
-docker push hamzazahid/multi-server
-docker push hamzazahid/multi-worker
+docker build -t hamzazahid/multi-client:latest -t hamzazahid/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t hamzazahid/multi-server:latest -t hamzazahid/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t hamzazahid/multi-worker:latest -t hamzazahid/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push hamzazahid/multi-client:latest
+docker push hamzazahid/multi-server:latest
+docker push hamzazahid/multi-worker:latest
+
+docker push hamzazahid/multi-client:$SHA
+docker push hamzazahid/multi-server:$SHA
+docker push hamzazahid/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=hamzazahid/multi-server
+kubectl set image deployments/server-deployment server=hamzazahid/multi-server:$SHA
+kubectl set image deployments/client-deployment client=hamzazahid/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=hamzazahid/multi-worker:$SHA
