7d66fc320e9704a17733fdaefe37622dff042c50
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2022-03-09 22:34:53 +0600

    update frontend

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,55 +1,57 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+/** @format */
+
+import axios from 'axios'
+import React, { Component } from 'react'
 
 class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
     index: ''
-  };
+  }
 
   componentDidMount() {
-    this.fetchValues();
-    this.fetchIndexes();
+    this.fetchValues()
+    this.fetchIndexes()
   }
 
   async fetchValues() {
-    const values = await axios.get('/api/values/current');
-    this.setState({ values: values.data });
+    const values = await axios.get('/api/values/current')
+    this.setState({ values: values.data })
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get('/api/values/all');
+    const seenIndexes = await axios.get('/api/values/all')
     this.setState({
       seenIndexes: seenIndexes.data
-    });
+    })
   }
 
-  handleSubmit = async event => {
-    event.preventDefault();
+  handleSubmit = async (event) => {
+    event.preventDefault()
 
     await axios.post('/api/values', {
       index: this.state.index
-    });
-    this.setState({ index: '' });
-  };
+    })
+    this.setState({ index: '' })
+  }
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    return this.state.seenIndexes.map(({ number }) => number).join(', ')
   }
 
   renderValues() {
-    const entries = [];
+    const entries = []
 
     for (let key in this.state.values) {
       entries.push(
         <div key={key}>
           For index {key} I calculated {this.state.values[key]}
         </div>
-      );
+      )
     }
 
-    return entries;
+    return entries
   }
 
   render() {
@@ -59,19 +61,19 @@ class Fib extends Component {
           <label>Enter your index:</label>
           <input
             value={this.state.index}
-            onChange={event => this.setState({ index: event.target.value })}
+            onChange={(event) => this.setState({ index: event.target.value })}
           />
           <button>Submit</button>
         </form>
 
-        <h3>Indexes I have seen:</h3>
+        <h3>Indexes I have seen: this is get from postgress</h3>
         {this.renderSeenIndexes()}
 
-        <h3>Calculated Values:</h3>
+        <h3>Calculated Values: this is get from redis</h3>
         {this.renderValues()}
       </div>
-    );
+    )
   }
 }
 
-export default Fib;
+export default Fib
